08da38cd7c99d73ccaa50b1f509a3e211bac339a
Marcos <marcosmarquitos132@gmail.com>
2023-03-24 13:07:40 +0100

    Feat: Cambio de navbar al logear + enlaces a perfil y funcion logout

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -1,15 +1,51 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Header.css'
 
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { userData, userout } from '../../pages/userSlice';
 
 export const Header = () => {
+
+  const dispatch = useDispatch();
+
+  const datosCredencialRdx = useSelector(userData);
+
+  const navigate = useNavigate();
+
+  useEffect(()=>{
+    console.log(datosCredencialRdx);
+  })
+//Funcion logout y redireccion a home
+  const logoutFunction = () => {
+    dispatch(userout({credentials: {}, token: ""}))
+
+    setTimeout(() => {
+      navigate("/");
+    }, 1000);
+  }
+
   return (
     <div className='headerDesign'>
-      <div className='navbarDesign'>
+      {datosCredencialRdx.credentials.token ? (
+        <div className='navbarDesign'>
+        <Navbar bg="dark" variant="dark" >
+          <Container>
+            <Navbar.Brand as={Link} to='/'><img src="/src/img/diente.png" className='imgNavbar'/>Clinica Grand Line</Navbar.Brand>
+            <Nav className="d-flex">
+              <Nav.Link as={Link} to='/profile'>
+                {datosCredencialRdx?.credentials?.usuario?.email}
+              </Nav.Link>
+              <Nav.Link onClick={logoutFunction}>Logout</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+      </div>
+      ) : (
+        <div className='navbarDesign'>
         <Navbar bg="dark" variant="dark" >
           <Container>
             <Navbar.Brand as={Link} to='/'><img src="/src/img/diente.png" className='imgNavbar'/>Clinica Grand Line</Navbar.Brand>
@@ -19,7 +55,8 @@ export const Header = () => {
             </Nav>
           </Container>
         </Navbar>
-      </div>
+        </div>
+      )}
     </div>
   )
 }
